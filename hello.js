9d5d4dc98aa70448989d6eb938c6ac58451a91bb
Vikash Kumar Chaubey <230101120283@centurionuniv.edu.in>
2026-06-18 11:47:21 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,14 @@
+// console.log("Hey there! I am JS")
+
+// console.log(window)  I will throw error out of browser
+
+// console.log(alert("Hey!")) It will also throw error
+
+// In package.json file we can make their own scripts like :- "start": "node hello.js" your file name then then directly you can npm start and automatically your will file will be run
+
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(2, 5));
