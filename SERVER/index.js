fefa0725f09041e1139ddd01ef6f35600f8ceb0e
Vikash Kumar Chaubey <230101120283@centurionuniv.edu.in>
2026-06-20 12:49:44 +0530

    http request and http methods

--- SERVER/index.js
+++ SERVER/index.js
@@ -9,16 +9,33 @@
 
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: ${req.url} New Request Received\n`
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: &{req.method} ${req.url} New Request Received\n`
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     // res.end("Hello from Server Again");
     switch(req.url) {
-      case '/': res.end("HomePage");
+      case '/': 
+      if (req.method === "GET") res.end("HomePage");
       break;
-      case '/about': res.end("I am Vikash");
+      case '/about': 
+      const username = myUrl.query.myname;
+      res.end(`Hi, ${username}`);
       break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("Here are you results for " + search);
+        
+        case "/signup":
+          if (req.method === "GET") res.end("This is a signup form");
+          else if (req.method === "POST") {
+            // DB query 
+            res.end("Success");
+          }
       default: res.end("404 Not Found");
     }
   })
